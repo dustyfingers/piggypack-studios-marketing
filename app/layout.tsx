0df882d8fcc4e41e193b4c4d97258b903aa48c89
lou <dustyfingers01257@gmail.com>
2024-07-06 15:25:39 -0600

    start on landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// custom logo font
+const logoFont = localFont({
+  src: "fonts/ElfEmmaAndTheTinySpider.otf",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={logoFont.className}>
+      <body>{children}</body>
     </html>
   );
 }
