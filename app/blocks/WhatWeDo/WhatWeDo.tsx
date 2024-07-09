60849e2bbfab8acf615fe2e315d41552cd5e33dd
lou <dustyfingers01257@gmail.com>
2024-07-08 23:01:34 -0600

    resize what we do block a bit

--- app/blocks/WhatWeDo/WhatWeDo.tsx
+++ app/blocks/WhatWeDo/WhatWeDo.tsx
@@ -50,8 +50,8 @@ const TextImageSection = ({
           <div className={style.borderContainer}>
             <Image
               src={imageSrc}
-              width={350}
-              height={350}
+              width={450}
+              height={450}
               alt="stock image of a guy working"
               className={`${style.image} ${
                 imageFirst ? style.imageFirst : style.contentFirst
