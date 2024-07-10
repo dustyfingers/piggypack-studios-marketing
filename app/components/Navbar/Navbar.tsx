d9243251a3905ec45831b0fb06ee061336503991
lou <dustyfingers01257@gmail.com>
2024-07-10 11:26:51 -0600

    work more on content container

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,22 +6,24 @@ const Navbar = () => {
   return (
     <nav className={style.navbar}>
       <ContentContainer>
-        <Link href="/">
-          <h6 className={style.logo}>PIGGYBACK</h6>
-        </Link>
-        <ul className={style.navMenu}>
-          <li className={style.navItem}>
-            <Link href="/about-us" className={style.navItemLink}>
-              About Us
-            </Link>
-          </li>
-          <li className={style.navItem}>
-            <Link href="/our-work">Our Work</Link>
-          </li>
-          <li className={style.navItem}>
-            <Link href="/work-with-us">Work With Us</Link>
-          </li>
-        </ul>
+        <div className={style.flexContainer}>
+          <Link href="/">
+            <h6 className={style.logo}>PIGGYBACK</h6>
+          </Link>
+          <ul className={style.navMenu}>
+            <li className={style.navItem}>
+              <Link href="/about-us" className={style.navItemLink}>
+                About Us
+              </Link>
+            </li>
+            <li className={style.navItem}>
+              <Link href="/our-work">Our Work</Link>
+            </li>
+            <li className={style.navItem}>
+              <Link href="/work-with-us">Work With Us</Link>
+            </li>
+          </ul>
+        </div>
       </ContentContainer>
     </nav>
   );
