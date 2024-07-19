643b8c2d4a7e7601fbaee24e585d18bb1fedfdbf
lou <dustyfingers01257@gmail.com>
2024-07-19 11:48:36 -0600

    try another approach to horizontal scroll area

--- app/blocks/HorizontalScrollArea/HorizontalScrollArea.tsx
+++ app/blocks/HorizontalScrollArea/HorizontalScrollArea.tsx
@@ -1,44 +1,44 @@
 "use client";
 import { useRef, useMemo } from "react";
-// import { useGSAP } from "@gsap/react";
-// import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 
 import style from "./component.module.css";
-// import { useScrollContext } from "@/app/context/Scroll";
+import { useScrollContext } from "@/app/context/Scroll";
 
 const HorizontalScrollArea = () => {
   const sectionRef = useRef<HTMLElement>(null);
   const scrollableAreaRef = useRef<HTMLDivElement>(null);
-  // const [scrollContext] = useScrollContext();
+  const [scrollContext] = useScrollContext();
 
-  // const horizontalScrollAreaLength = useMemo(() => {
-  //   if (scrollableAreaRef.current) return scrollableAreaRef.current.offsetWidth;
-  //   else return 0;
-  // }, [scrollableAreaRef.current]);
-  // let horizontalScrollLength: number = 0;
-  // if (typeof window !== "undefined")
-  //   horizontalScrollLength = horizontalScrollAreaLength - window.innerWidth;
+  const horizontalScrollAreaLength = useMemo(() => {
+    if (scrollableAreaRef.current) return scrollableAreaRef.current.offsetWidth;
+    else return 0;
+  }, [scrollableAreaRef.current]);
+  let horizontalScrollLength: number = 0;
+  if (typeof window !== "undefined")
+    horizontalScrollLength = horizontalScrollAreaLength - window.innerWidth;
 
-  // useGSAP(
-  //   () => {
-  //     if (scrollContext.scrollContainer) {
-  //       gsap.to(style.scrollableArea, {
-  //         scrollTrigger: {
-  //           scroller: scrollContext.scrollContainer, //locomotive-scroll container
-  //           scrub: true,
-  //           trigger: style.horizontalScrollSection,
-  //           pin: true,
-  //           anticipatePin: 1,
-  //           start: "top top",
-  //           end: horizontalScrollAreaLength,
-  //         },
-  //         x: -horizontalScrollLength,
-  //         ease: "none",
-  //       });
-  //     }
-  //   },
-  //   { scope: sectionRef }
-  // );
+  useGSAP(
+    () => {
+      if (scrollContext.scrollContainer) {
+        gsap.to(style.scrollableArea, {
+          scrollTrigger: {
+            scroller: scrollContext.scrollContainer, //locomotive-scroll container
+            scrub: true,
+            trigger: style.horizontalScrollSection,
+            pin: true,
+            anticipatePin: 1,
+            start: "top top",
+            end: horizontalScrollAreaLength,
+          },
+          x: -horizontalScrollLength,
+          ease: "none",
+        });
+      }
+    },
+    { scope: sectionRef }
+  );
 
   return (
     <section
