505c4ac63c3abf7b0c224672151f75c167b5fb7e
lou <dustyfingers01257@gmail.com>
2024-07-23 01:20:42 -0600

    make hamburger icon a bit smaller

--- app/components/svg/AnimatedHamburgerIcon/AnimatedHamburgerIcon.tsx
+++ app/components/svg/AnimatedHamburgerIcon/AnimatedHamburgerIcon.tsx
@@ -3,7 +3,7 @@ import { useMobileNavMenuContext } from "@/app/context/MobileNavMenu";
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const hamburgerHeight = 20;
+const hamburgerHeight = 16;
 
 const hamburgerWidth = Math.round(hamburgerHeight * 1.33);
 const stripeHeight = hamburgerHeight / 5;
