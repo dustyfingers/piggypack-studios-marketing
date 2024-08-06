0013eb2c586dbd3b6e3bc25d403e4cac1c3390ec
lou <dustyfingers01257@gmail.com>
2024-08-06 12:30:25 -0600

    work more on contact form, rework landing hero after design critique

--- app/blocks/ContactForm/ContactForm.tsx
+++ app/blocks/ContactForm/ContactForm.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { useState } from "react";
 import ReactiveButton from "reactive-button";
-import { SubmitHandler, useForm } from "react-hook-form";
+import {
+  FieldErrors,
+  SubmitHandler,
+  useForm,
+  UseFormRegister,
+} from "react-hook-form";
 
 import style from "./component.module.css";
 import ContentContainer from "@/app/components/ContentContainer/ContentContainer";
@@ -10,6 +15,106 @@ type Inputs = {
   name: string;
   email: string;
   message: string;
+};
+
+interface IFormInputInfo {
+  label: string;
+  value: string;
+  placeholder: string;
+}
+
+const formInputInfo: IFormInputInfo[] = [
+  { label: "Name", value: "name", placeholder: "John A. Smith" },
+  { label: "Email", value: "email", placeholder: "johnsmith@email.com" },
+  {
+    label: "Message",
+    value: "message",
+    placeholder: "Tell us more about what you need...",
+  },
+];
+
+interface IFormInput {
+  label: string;
+  value: string;
+  placeholder: string;
+  register: UseFormRegister<Inputs>;
+  errors: FieldErrors<Inputs>;
+}
+
+const FormInput = ({
+  label,
+  value,
+  placeholder,
+  register,
+  errors,
+}: IFormInput) => {
+  return (
+    <label className={style.formInputContainer}>
+      <span className={style.formLabel}>{label}:</span>
+      <input
+        {...register(value as "name" | "email" | "message", { required: true })}
+        className={style.formInput}
+        placeholder={placeholder}
+      />
+      {errors.name && <span className={style.formError}>Name is required</span>}
+    </label>
+  );
+};
+
+interface IGridInfoItem {
+  label: string;
+  value: string;
+}
+
+const interestsGridInfo: IGridInfoItem[] = [
+  { label: "E-Commerce Site", value: "E-Commerce Site" },
+  { label: "Brand Development", value: "Brand Development" },
+  {
+    label: "Website Upgrades, Hosting and Maintenance",
+    value: "Website Upgrades, Hosting and Maintenance",
+  },
+  {
+    label: "Social Media Marketing and Management",
+    value: "Social Media Marketing and Management",
+  },
+  { label: "Marketing Site", value: "Marketing Site" },
+  { label: "Not Sure", value: "Not Sure" },
+];
+
+const budgetGridInfo: IGridInfoItem[] = [
+  { label: "<5K", value: "<5K" },
+  { label: "5K-10K", value: "5K-10K" },
+  { label: "10K-25K", value: "10K-25K" },
+  { label: "25K-50K", value: "25K-50K" },
+  { label: "50K+", value: "50K+" },
+];
+
+interface IGridButton {
+  label: string;
+  value: string;
+  stateValue: string | string[] | undefined;
+  handleChange: (
+    val: string
+  ) => void | React.Dispatch<React.SetStateAction<string>>;
+}
+
+const GridButton = ({
+  label,
+  value,
+  stateValue,
+  handleChange,
+}: IGridButton) => {
+  return (
+    <button
+      type="button"
+      className={`${style.gridButton} ${
+        stateValue === value ? style.activeGridButton : ""
+      }`}
+      onClick={() => handleChange(value)}
+    >
+      {label}
+    </button>
+  );
 };
 
 const ContactForm = () => {
@@ -22,6 +127,7 @@ const ContactForm = () => {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     setButtonState("loading");
+    console.log(data);
     const res = await fetch("/api/work-with-us", {
       method: "POST",
       body: JSON.stringify(data),
@@ -34,7 +140,6 @@ const ContactForm = () => {
     } catch (err) {
       console.log({ err });
     }
-    console.log(data);
   };
 
   const [interestItems, setInterestItems] = useState<string[]>([]);
@@ -52,162 +157,28 @@ const ContactForm = () => {
     <section className={style.formContainer}>
       <ContentContainer>
         <form className={style.contactForm} onSubmit={handleSubmit(onSubmit)}>
-          <label className={style.formInputContainer}>
-            <span className={style.formLabel}>Name:</span>
-            <input
-              {...register("name", { required: true })}
-              className={style.formInput}
-              placeholder="John Smith"
-            />
-            {errors.name && (
-              <span className={style.formError}>Name is required</span>
-            )}
-          </label>
-
-          <label className={style.formInputContainer}>
-            <span className={style.formLabel}>Email:</span>
-            <input
-              type="email"
-              {...register("email", { required: true })}
-              className={style.formInput}
-              placeholder="johnsmith@email.com"
-            />
-            {errors.email && (
-              <span className={style.formError}>Email is required</span>
-            )}
-          </label>
-
-          <label className={style.formInputContainer}>
-            <span className={style.formLabel}>Message:</span>
-            <input
-              type="textarea"
-              {...register("message", { required: true })}
-              className={style.formInput}
-              placeholder="Tell us about your project..."
-            />
-            {errors.message && (
-              <span className={style.formError}>Message is required</span>
-            )}
-          </label>
-
-          {/* types of service interested in */}
+          {formInputInfo.map((info) => (
+            <FormInput {...info} register={register} errors={errors} />
+          ))}
           <label className={style.gridLabel}>I am looking for...</label>
           <div className={style.interestGrid}>
-            <button
-              type="button"
-              onClick={() => handleInterestItemsChange("E-Commerce Site")}
-              className={`${style.gridButton} ${
-                interestItems.includes("E-Commerce Site")
-                  ? style.activeGridButton
-                  : ""
-              }`}
-            >
-              E-Commerce Site
-            </button>
-            <button
-              type="button"
-              onClick={() => handleInterestItemsChange("Brand Development")}
-              className={`${style.gridButton} ${
-                interestItems.includes("Brand Development")
-                  ? style.activeGridButton
-                  : ""
-              }`}
-            >
-              Brand Development
-            </button>
-            <button
-              type="button"
-              onClick={() =>
-                handleInterestItemsChange(
-                  "Website Upgrades, Hosting and Maintenance"
-                )
-              }
-              className={`${style.gridButton} ${
-                interestItems.includes(
-                  "Website Upgrades, Hosting and Maintenance"
-                )
-                  ? style.activeGridButton
-                  : ""
-              }`}
-            >
-              Website Upgrades, Hosting and Maintenance
-            </button>
-            <button
-              type="button"
-              onClick={() =>
-                handleInterestItemsChange(
-                  "Social Media Marketing and Management"
-                )
-              }
-              className={`${style.gridButton} ${
-                interestItems.includes("Social Media Marketing and Management")
-                  ? style.activeGridButton
-                  : ""
-              }`}
-            >
-              Social Media Marketing and Management
-            </button>
-            <button
-              type="button"
-              value=""
-              onClick={() => handleInterestItemsChange("Marketing Site")}
-              className={`${style.gridButton} ${
-                interestItems.includes("Marketing Site")
-                  ? style.activeGridButton
-                  : ""
-              }`}
-            >
-              Marketing Site
-            </button>
-            <button
-              type="button"
-              onClick={() => handleInterestItemsChange("Not Sure")}
-              className={`${style.gridButton} ${
-                interestItems.includes("Not Sure") ? style.activeGridButton : ""
-              }`}
-            >
-              Not Sure
-            </button>
+            {interestsGridInfo.map((info) => (
+              <GridButton
+                {...info}
+                stateValue={interestItems}
+                handleChange={handleInterestItemsChange}
+              />
+            ))}
           </div>
-          {/* estimated budget */}
           <label className={style.gridLabel}>My budget is around...</label>
           <div className={style.budgetGrid}>
-            <button
-              type="button"
-              className={`${style.gridButton} ${
-                budget === "<5K" ? style.activeGridButton : ""
-              }`}
-              onClick={() => setBudget("<5K")}
-            >
-              &gt;5K
-            </button>
-            <button
-              type="button"
-              className={`${style.gridButton} ${
-                budget === "5K-10K" ? style.activeGridButton : ""
-              }`}
-              onClick={() => setBudget("5K-10K")}
-            >
-              5K-10K
-            </button>
-            <button
-              type="button"
-              className={`${style.gridButton} ${
-                budget === "10K-25K" ? style.activeGridButton : ""
-              }`}
-              onClick={() => setBudget("10K-25K")}
-            >
-              10K-25K
-            </button>
-            <button
-              type="button"
-              className={`${style.gridButton} ${
-                budget === "25K+" ? style.activeGridButton : ""
-              }`}
-              onClick={() => setBudget("25K+")}
-            >
-              25K+
-            </button>
+            {budgetGridInfo.map((info) => (
+              <GridButton
+                {...info}
+                stateValue={budget}
+                handleChange={setBudget}
+              />
+            ))}
           </div>
           <ReactiveButton
             outline
@@ -217,7 +188,6 @@ const ContactForm = () => {
             loadingText="loading..."
             successText="success!"
             className={style.submitButton}
-            // block={false}
           />
         </form>
       </ContentContainer>
--- app/blocks/LandingHero/LandingHero.tsx
+++ app/blocks/LandingHero/LandingHero.tsx
@@ -2,16 +2,25 @@
 import styles from "./component.module.css";
 
 import ContentContainer from "@/app/components/ContentContainer/ContentContainer";
-import LandingTypeLogo from "@/app/components/LandingTypeLogo";
+import LandingTypeLogo from "@/app/components/svg/LandingTypeLogo";
 
 const LandingHero = () => {
   return (
     <section className={styles.landingHero}>
+      <video
+        autoPlay
+        loop
+        muted
+        className={styles.bgVideo}
+        width="320"
+        height="240"
+        preload="none"
+      >
+        <source src={"/videos/landing-hero-test-vid.mp4"} type="video/mp4" />
+      </video>
       <ContentContainer>
         <div className={styles.landingGrid}>
-          <div className={styles.leadContainer}>
-            <LandingTypeLogo />
-          </div>
+          <LandingTypeLogo />
           <p className={styles.contentContainer}>
             Piggyback Studios is a
             <strong> boutique design and development</strong> agency based in
