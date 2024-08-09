a0a5704cd6d5cf92caedabf6765dfb6fc820bca2
lou <dustyfingers01257@gmail.com>
2024-08-08 21:44:45 -0600

    Work on made in colorado block, about us block, our impact block, and page hero

--- app/blocks/AboutUs/AboutUs.tsx
+++ app/blocks/AboutUs/AboutUs.tsx
@@ -1,10 +1,49 @@
 import ContentContainer from "@/app/components/ContentContainer/ContentContainer";
 import style from "./component.module.css";
+import Image from "next/image";
 
 const AboutUs = () => {
   return (
     <section className={style.aboutUs}>
-      <ContentContainer>About Us</ContentContainer>
+      <div className={style.firstHalf}></div>
+      <div className={style.secondHalf}></div>
+      <ContentContainer>
+        <h2 className={style.aboutUsHeader}>About Us</h2>
+        <div className={style.contentGrid}>
+          <div className={style.firstContent}>
+            <Image
+              src="/images/stock-image.jpg"
+              alt="a guy working..."
+              width={256}
+              height={256}
+            />
+            <h4>Louie Williford</h4>
+            <h6>Founding Partner, Technical Director</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ex
+              quas officia eum consectetur dolor maxime illum itaque totam
+              consequuntur, asperiores earum, quidem iure atque provident
+              perferendis! Asperiores, recusandae officiis.
+            </p>
+          </div>
+          <div className={style.secondContent}>
+            <Image
+              src="/images/stock-image.jpg"
+              alt="a guy working..."
+              width={256}
+              height={256}
+            />
+            <h4>Katy Williford</h4>
+            <h6>Founding Partner, Creative Director</h6>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ex
+              quas officia eum consectetur dolor maxime illum itaque totam
+              consequuntur, asperiores earum, quidem iure atque provident
+              perferendis! Asperiores, recusandae officiis.
+            </p>
+          </div>
+        </div>
+      </ContentContainer>
     </section>
   );
 };
--- app/blocks/OurImpact/OurImpact.tsx
+++ app/blocks/OurImpact/OurImpact.tsx
@@ -1,15 +1,87 @@
 import ContentContainer from "@/app/components/ContentContainer";
 import style from "./component.module.css";
 
+const LeadsIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="48px"
+      viewBox="0 -960 960 960"
+      width="48px"
+      fill="var(--cream)"
+      className={style.impactIcon}
+    >
+      <path d="M120-120v-76l60-60v136h-60Zm165 0v-236l60-60v296h-60Zm165 0v-296l60 61v235h-60Zm165 0v-235l60-60v295h-60Zm165 0v-396l60-60v456h-60ZM120-356v-85l280-278 160 160 280-281v85L560-474 400-634 120-356Z" />
+    </svg>
+  );
+};
+
+const SalesIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="48px"
+      viewBox="0 -960 960 960"
+      width="48px"
+      fill="var(--cream)"
+      className={style.impactIcon}
+    >
+      <path d="M863-404 557-97q-9 8.5-20.25 12.75T514.25-80Q503-80 492-84.5T472-97L98-472q-8-8-13-18.96-5-10.95-5-23.04v-306q0-24.75 17.63-42.38Q115.25-880 140-880h307q12.07 0 23.39 4.87Q481.7-870.25 490-862l373 373q9.39 9 13.7 20.25 4.3 11.25 4.3 22.5t-4.5 22.75Q872-412 863-404ZM516-138l306-307-375-375H140v304l376 378ZM245-664q21 0 36.5-15.5T297-716q0-21-15.5-36.5T245-768q-21 0-36.5 15.5T193-716q0 21 15.5 36.5T245-664Zm236 185Z" />
+    </svg>
+  );
+};
+
+const BrandVisibilityIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="48px"
+      viewBox="0 -960 960 960"
+      width="48px"
+      fill="var(--cream)"
+      className={style.impactIcon}
+    >
+      <path d="M480.12-330q70.88 0 120.38-49.62t49.5-120.5q0-70.88-49.62-120.38T479.88-670Q409-670 359.5-620.38T310-499.88q0 70.88 49.62 120.38t120.5 49.5Zm-.36-58q-46.76 0-79.26-32.74-32.5-32.73-32.5-79.5 0-46.76 32.74-79.26 32.73-32.5 79.5-32.5 46.76 0 79.26 32.74 32.5 32.73 32.5 79.5 0 46.76-32.74 79.26-32.73 32.5-79.5 32.5Zm.24 188q-146 0-264-83T40-500q58-134 176-217t264-83q146 0 264 83t176 217q-58 134-176 217t-264 83Zm0-300Zm-.17 240Q601-260 702.5-325.5 804-391 857-500q-53-109-154.33-174.5Q601.34-740 480.17-740T257.5-674.5Q156-609 102-500q54 109 155.33 174.5Q358.66-260 479.83-260Z" />
+    </svg>
+  );
+};
+
 const OurImpact = () => {
   return (
     <section className={style.impactSection}>
       <ContentContainer>
-        <h3>Our Impact</h3>
+        <h2 className={style.impactHeader}>Our Impact</h2>
         <div className={style.impactGrid}>
-          <div className={style.impactItem}>Increased Leads</div>
-          <div className={style.impactItem}>Increased Sales and Bookings</div>
-          <div className={style.impactItem}>Increased Brand Visibility</div>
+          <div className={style.impactGridItem}>
+            <LeadsIcon />
+            <h4>Increased Leads</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              soluta earum minima rem cupiditate nostrum, magni neque quasi
+              saepe. Obcaecati, ut odio! Repellendus odit impedit facilis
+              provident delectus quis ut!
+            </p>
+          </div>
+          <div className={style.impactGridItem}>
+            <SalesIcon />
+            <h4>Increased Sales and Bookings</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              soluta earum minima rem cupiditate nostrum, magni neque quasi
+              saepe. Obcaecati, ut odio! Repellendus odit impedit facilis
+              provident delectus quis ut!
+            </p>
+          </div>
+          <div className={style.impactGridItem}>
+            <BrandVisibilityIcon />
+            <h4>Increased Brand Visibility</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+              soluta earum minima rem cupiditate nostrum, magni neque quasi
+              saepe. Obcaecati, ut odio! Repellendus odit impedit facilis
+              provident delectus quis ut!
+            </p>
+          </div>
         </div>
       </ContentContainer>
     </section>
